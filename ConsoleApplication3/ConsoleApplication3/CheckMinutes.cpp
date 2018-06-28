3309200d0abffe8848b6242a56dbcbe6b1852410
KNOWN-UNIVERSE\miapata <miapata@uat.edu>
2018-06-27 18:41:01 -0700

    f

---
+++ ConsoleApplication3/ConsoleApplication3/CheckMinutes.cpp
@@ -0,0 +1,14 @@
+#include"CheckMinutes.h"
+#include "stdafx.h"
+using namespace std;
+bool CheckMinutes(int minutes) {
+	if (minutes == NULL) {
+
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+
+}
