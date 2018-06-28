535bedab8822ad22c097cbc2d50882d75adef146
Miangelp <miapata@uat.edu>
2018-06-27 23:44:36 -0700

    Comm

--- ConsoleApplication3/ConsoleApplication3/CheckMinutes.cpp
+++ ConsoleApplication3/ConsoleApplication3/CheckMinutes.cpp
@@ -1,13 +1,20 @@
 #include"CheckMinutes.h"
 #include "stdafx.h"
-using namespace std;
-bool CheckMinutes(int minutes) {
-	if (minutes == NULL) {
 
+using namespace std;
+
+//This bool is used to check if the user input is valid
+bool CheckMinutes(int minutes) {
+	//If minutes are null or 0
+	if (minutes == NULL) {
+		//Return as false
 		return false;
 	}
+
+	//If it's anything else
 	else
 	{
+		//Return true
 		return true;
 	}
 
--- ConsoleApplication3/ConsoleApplication3/CheckMinutes.h
+++ ConsoleApplication3/ConsoleApplication3/CheckMinutes.h
@@ -1,2 +1,3 @@
 #pragma once
+//This is a boolean function for the input checking
  bool CheckMinutes(int minutes);
