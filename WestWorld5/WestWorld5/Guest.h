f5c08de8b5711d839b4ac36a5bded55500ae9bf8
KNOWN-UNIVERSE\miapata <miapata@uat.edu>
2018-06-27 18:24:51 -0700

    Jesus

--- WestWorld5/WestWorld5/Guest.h
+++ WestWorld5/WestWorld5/Guest.h
@@ -2,6 +2,7 @@
 #include "stdafx.h"
 #include "Denizen.h"
 
+
 class Guest: Denizen
 {
 public :
---
+++ Your Chair is Killing You/Your Chair is Killing You/RandomName.cpp
@@ -0,0 +1,11 @@
+#include "RandomName.h"
+#include<ctime>
+#include"stdafx.h"
+#include <string>
+using namespace std;
+
+//Get a random name from a function
+
+string RandomName:: GetName() {
+
+}
